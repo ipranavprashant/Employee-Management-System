c8e567cbc63d67bfdffef6b6ce0d498460e8d479
Pranav Prashant <97225953+ipranavprashant@users.noreply.github.com>
2023-02-17 17:57:35 +0530

    Update Employee_Management_System.cpp

--- Employee_Management_System.cpp
+++ Employee_Management_System.cpp
@@ -243,7 +243,6 @@ void deleteEmployee(struct employee e[], int size, int id)
 
 int main()
 {
-    cout << "\n**This Program is made by Pranav Prashant, BTech Computer Science and Engineering at National Institute of Technology Calicut as the major project under the training cum intership program of the Externsclub internship program(October-December Batch)-2022**" << endl;
     cout << "\nSome General Instructions/Restrictions to use this Employee Management System," << endl
          << "1)The Program is well instructed so do read the screen before clicking on any option(s).\n2)The Program is case sensitive.\n3)You can search the employee details based on multiple parameters.\n4)Enter only the first name of the Employee.\n5)The parameter 'designation' is of the type character, eg:Teacher,Assistant,developer,director etc..\n6)You can delete the data with only the employee id, this was done on purpose as deletion is a very sensitive case so deleting it with primary key made sense i.e; the Employee id in our case which uniquely identifies each employee.\n\nPS:However with a very minimal tweak the deletion can also be done based on any parameter.\n   Do Inform me if there are any improvements that can be made in this program.\n   Contact me at pranav_b210460cs@nitc.ac.in\n";
     char ch;
